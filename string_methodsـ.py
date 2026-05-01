ece9c034d08b645b7e8d80f4289dd6df5655798b
Arshak <189806520+arshakkarami@users.noreply.github.com>
2026-04-30 21:08:00 -0400

    add string methods practice

---
+++ string_methodsـ.py
@@ -0,0 +1,12 @@
+name = "Arshak"
+
+print(len(name))
+print(name.find("A"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("s"))
+print(name.replace("s","a"))
+print(name*3)

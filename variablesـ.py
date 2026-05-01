af65f6b64d97183e9be79043446a5d5b9db8a3aa
Arshak <189806520+arshakkarami@users.noreply.github.com>
2026-04-30 21:06:51 -0400

    add variables practice
    Define variables of different types and print their values.

---
+++ variablesـ.py
@@ -0,0 +1,42 @@
+
+full_name = first_name + " "+last_name
+
+STRING
+
+print("Hello " + full_name)
+print(type(name))
+
+INT
+
+age = 21
+age += 1
+print("Your age is: "+str(age))
+print(type(age))
+
+###FLOAT
+height = 250.5
+print("your height is: " +str(height) + "cm")
+
+###BOOLEAN
+human = False
+print("Are you a human:"+ str(human))
+
+###BOOLEAN
+human = True
+print("Are you a human:"+ str(human))
+
+name = "ARSHAK"
+age = 17
+attractive = True
+
+name, age, attractive = "Arshak", 18, True
+print(name)
+print(age)
+print(attractive)
+
+spongebob = patrick = sandy = squidward = 20
+
+print(spongebob)
+print(patrick)
+print(sandy)
+print(squidward)
